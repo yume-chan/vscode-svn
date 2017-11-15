c950a1c2257870c15bcc8fb5c4271c81c0a059ed
Simon Chan <cnsimonchan@live.com>
2017-11-15 18:01:33 +0800

    Better loading

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 import { ExtensionContext } from "vscode";
 
-import { client } from "./client";
 import { commandCenter } from "./command-center";
 import { svnTextDocumentContentProvider } from "./content-provider";
-import { SvnResourceState, SvnSourceControl } from "./svn-source-control";
 import { workspaceManager } from "./workspace-manager";
 
 export function activate(context: ExtensionContext) {
